0615f477aec091d4d7a7b554f2db2beaa0fba3cd
andres.cuervo <andres.cuervo@tecnilogica.com>
2016-05-30 12:53:56 +0200

    prettify code

--- scripts.js
+++ scripts.js
@@ -1,28 +1,26 @@
-$(document).ready(function(){
-	getQuote();
+$(document).ready(function() {
+    getQuote();
 
-	$('#new-quote').on('click', function(){
-		getQuote();
-	})
+    $('#new-quote').on('click', function() {
+        getQuote();
+    })
 });
 
-function getQuote(){
+function getQuote() {
 
 
-	$.ajax({
-		headers: {
-			"X-Mashape-Key": "oLEOWxghNfmshxtjEwSaJVoPEvCgp1eonWIjsn9OND95lOePpL"
-		},
-		url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
-		method: "post",
-		success: function(response){
-			var r = JSON.parse(response);
+    $.ajax({
+        headers: {
+            "X-Mashape-Key": "oLEOWxghNfmshxtjEwSaJVoPEvCgp1eonWIjsn9OND95lOePpL"
+        },
+        url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+        method: "post",
+        success: function(response) {
+            var r = JSON.parse(response);
 
-console.log(r);
+            $('#quoteParagraph').html(r.quote);
+            $('#quoteAuthor').html(r.author);
+        }
 
-			$('#quoteParagraph').html(r.quote);
-			$('#quoteAuthor').html(r.author);
-		}
-
-	});
+    });
 }
